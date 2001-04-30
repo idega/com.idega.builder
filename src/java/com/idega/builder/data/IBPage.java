e47cf15650ec33f57ad9dec1ca23e2d37a23c675
Pall Helgason <palli@idega.com>
2001-04-30 16:40:40 +0000

    Fix all but two deprication errors in com tree

--- src/java/com/idega/builder/data/IBPage.java
+++ src/java/com/idega/builder/data/IBPage.java
@@ -1,47 +1,46 @@
-//idega 2001 - Tryggvi Larusson
 /*
-*Copyright 2001 idega.is All Rights Reserved.
-*/
+ * $Id: IBPage.java,v 1.2 2001/04/30 16:40:40 palli Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
 package com.idega.builder.data;
 
-//import java.util.*;
 import java.sql.*;
 import com.idega.data.*;
-
 
 /**
 *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
 *@version 1.3
 */
-public class IBPage extends GenericEntity{
-
-	public IBPage(){
+public class IBPage extends GenericEntity {
+	public IBPage() {
 		super();
 	}
 
-	public IBPage(int id)throws SQLException{
+	public IBPage(int id)throws SQLException {
 		super(id);
 	}
 
-	public void initializeAttributes(){
-
+	public void initializeAttributes() {
 		//par1: column name, par2: visible column name, par3-par4: editable/showable, par5 ...
 		addAttribute(getIDColumnName());
 		addAttribute("name","Nafn",true,true,"java.lang.String");
                 //addAttribute("xmlvalue","XML",true,true,"com.idega.data.BlobWrapper");
-
 	}
 
-	public String getEntityName(){
+	public String getEntityName() {
 		return "ib_page";
 	}
 
-	public void setDefaultValues(){
+	public void setDefaultValues() {
 		//setColumn("image_id",1);
 	}
 
-	public String getName(){
+	public String getName() {
 		return getStringColumnValue("name");
 	}
-
 }
--- src/java/com/idega/builder/servlet/IBMainServlet.java
+++ src/java/com/idega/builder/servlet/IBMainServlet.java
@@ -1,11 +1,14 @@
-
-//idega 2001 - Tryggvi Larusson
 /*
-*Copyright 2001 idega.is All Rights Reserved.
-*/
+ * $Id: IBMainServlet.java,v 1.2 2001/04/30 16:40:40 palli Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
 package com.idega.builder.servlet;
 
-//import java.util.*;
 import com.idega.jmodule.*;
 import com.idega.data.*;
 import com.idega.jmodule.news.presentation.*;
@@ -28,200 +31,179 @@ import javax.servlet.ServletException;
 *@version 0.5 alpha
 */
 
-public class IBMainServlet extends JSPModule{
+public class IBMainServlet extends JSPModule {
 
-
-	public void __theService(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException,IOException{
-                try{
-                  main(getModuleInfo());
-                }
-                catch(SQLException ex){
-                  ex.printStackTrace(System.err);
-                }
-                //_jspService(request,response);
+	public void __theService(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+    try {
+      main(getModuleInfo());
+    }
+    catch(SQLException ex) {
+      ex.printStackTrace(System.err);
+    }
+    //_jspService(request,response);
 	}
 
+  public void main(ModuleInfo modinfo)throws IOException,SQLException {
+    //boolean isAdmin = AccessControl.isAdmin(modinfo);
+    boolean isAdmin = true;
+    PrintWriter debugger = modinfo.getResponse().getWriter();
+    int id;
 
+    String page_id = modinfo.getParameter("idegaweb_page_id");
+    if(page_id == null){
+      id = 1;
+    }
+    else {
+      id = Integer.parseInt(page_id);
+    }
 
-        public void main(ModuleInfo modinfo)throws IOException,SQLException{
+    String language = modinfo.getParameter("language");
+    if(language == null){
+      language = "IS";
+    }
 
-          //boolean isAdmin=AccessControl.isAdmin(modinfo);
-          boolean isAdmin=true;
+    IBObjectInstance[] instances;
+    IBPage ib_page = new IBPage(id);
+    IBAdminWindow window = new IBAdminWindow();
 
-          PrintWriter debugger = modinfo.getResponse().getWriter();
-
-          int id;
-
-          String page_id=modinfo.getParameter("idegaweb_page_id");
-          if(page_id == null){
-            id=1;
+    try {
+      instances = (IBObjectInstance[]) ib_page.findReverseRelated(new IBObjectInstance());
+      for(int i = 0; i < instances.length; i++) {
+        Table moduleTable = null;
+        Table toolbarTable = null;
+        if (isAdmin) {
+          moduleTable = new Table(1,2);
+          toolbarTable = new Table(3,1);
+          toolbarTable.setHeight(10);
+          moduleTable.add(toolbarTable,1,1);
+          moduleTable.setColor("gray");
+          moduleTable.setColor(1,2,"white");
+          moduleTable.setHeight(1,"10");
+          //moduleTable.setCellpadding(0);
+          //moduleTable.setCellspacing(0);
+        }
+        try {
+          ModuleObject obj = getSetObject(instances[i]);
+          if (isAdmin) {
+            moduleTable.add(obj,1,2);
+            //Text text = new Text(obj.getClass().getName());
+            //text.setFontSize(1);
+            //text.setFontColor("white");
+            //moduleTable.add(text,1,1);
           }
-          else{
-            id=Integer.parseInt(page_id);
+          else {
+            add(obj);
           }
+          //table.add(instances[i].getNewInstance());
+        }
+        catch(Exception ex) {
+          debugger.print("Error");
+        }
 
-          String language = modinfo.getParameter("language");
-          if(language== null){
-            language="IS";
-          }
+        if (isAdmin) {
+          /*Form form1 = new Form(new Window("Breyta","window2.jsp"));
+          form1.add(new SubmitButton(new Image("/pics/arachnea/change.gif")));
+          form1.add(new Parameter("ib_object_instance_id",""+instances[i].getID()));*/
 
+          AdminButton form2 = new AdminButton(new Image("/common/pics/arachnea/change.gif"),window);
+          form2.addParameter("ib_window_action","window2");
+          form2.addParameter(new Parameter("ib_object_instance_id",""+instances[i].getID()));
 
-          IBObjectInstance[] instances;
-          IBPage ib_page = new IBPage(id);
-          IBAdminWindow window = new IBAdminWindow();
+          toolbarTable.add(form2,1,1);
 
-          try{
-                  instances = (IBObjectInstance[]) ib_page.findReverseRelated(new IBObjectInstance());
-                  for(int i=0;i<instances.length;i++){
-                          Table moduleTable=null;
-                          Table toolbarTable=null;
-                          if(isAdmin){
-                            moduleTable = new Table(1,2);
-                            toolbarTable = new Table(3,1);
-                            toolbarTable.setHeight(10);
-                            moduleTable.add(toolbarTable,1,1);
+          /*Form form2 = new Form(new Window("Eyda","window3.jsp"));
+          form2.add(new SubmitButton(new Image("/pics/arachnea/delete.gif")));
+          form2.add(new Parameter("ib_object_instance_id",""+instances[i].getID()));*/
+          AdminButton form3 = new AdminButton(new Image("/common/pics/arachnea/delete.gif"),window);
+          form3.addParameter("ib_window_action","window3");
+          form3.addParameter(new Parameter("ib_object_instance_id",""+instances[i].getID()));
 
-                            moduleTable.setColor("gray");
-                            moduleTable.setColor(1,2,"white");
-                            moduleTable.setHeight(1,"10");
-                            //moduleTable.setCellpadding(0);
-                            //moduleTable.setCellspacing(0);
+          toolbarTable.add(form3,2,1);
 
-                          }
-                          try{
-                            ModuleObject obj = getSetObject(instances[i]);
-                            if(isAdmin){
-                              moduleTable.add(obj,1,2);
-                              //Text text = new Text(obj.getClass().getName());
-                              //text.setFontSize(1);
-                              //text.setFontColor("white");
-                              //moduleTable.add(text,1,1);
-                            }
-                            else{
-                              add(obj);
-                            }
-                                  //table.add(instances[i].getNewInstance());
-                          }
-                          catch(Exception ex){
-                                  debugger.print("Error");
-                          }
+          /*Form form3 = new Form(new Window("Stilla","window4.jsp"));
+          form3.add(new SubmitButton(new Image("/pics/arachnea/edit.gif")));
+          form3.add(new Parameter("ib_object_instance_id",""+instances[i].getID()));*/
+          AdminButton form4 = new AdminButton(new Image("/common/pics/arachnea/edit.gif"),window);
+          form4.addParameter("ib_window_action","window4");
+          form4.addParameter(new Parameter("ib_object_instance_id",""+instances[i].getID()));
 
-                          if(isAdmin){
+          toolbarTable.add(form4,3,1);
+          super.add(moduleTable);
+        }
+      }
+    }
+    catch(Exception ex) {
+      add("villa 1");
+      System.err.println("ERROR!!!!!!");
+      ex.printStackTrace(modinfo.getResponse().getWriter());
+    }
 
+    //AdminButton form = new AdminButton("Bæta við",window);
+    if (isAdmin) {
+      AdminButton form = new AdminButton(new Image("/common/pics/arachnea/add.gif"),window);
+      form.addParameter("ib_window_action","window1");
+      form.addParameter(new Parameter("page_id",Integer.toString(id)));
 
+      /*Form form = new Form(new Window("Baeta","window1.jsp"));
+      form.add(new SubmitButton("Bæta við"));
+      form.add(new Parameter("page_id",""+id));*/
 
-
-
-                            /*Form form1 = new Form(new Window("Breyta","window2.jsp"));
-                            form1.add(new SubmitButton(new Image("/pics/arachnea/change.gif")));
-                            form1.add(new Parameter("ib_object_instance_id",""+instances[i].getID()));*/
-
-                            AdminButton form2 = new AdminButton(new Image("/common/pics/arachnea/change.gif"),window);
-                            form2.addParameter("ib_window_action","window2");
-                            form2.addParameter(new Parameter("ib_object_instance_id",""+instances[i].getID()));
-
-                            toolbarTable.add(form2,1,1);
-
-                            /*Form form2 = new Form(new Window("Eyda","window3.jsp"));
-                            form2.add(new SubmitButton(new Image("/pics/arachnea/delete.gif")));
-                            form2.add(new Parameter("ib_object_instance_id",""+instances[i].getID()));*/
-                            AdminButton form3 = new AdminButton(new Image("/common/pics/arachnea/delete.gif"),window);
-                            form3.addParameter("ib_window_action","window3");
-                            form3.addParameter(new Parameter("ib_object_instance_id",""+instances[i].getID()));
-
-                            toolbarTable.add(form3,2,1);
-
-                            /*Form form3 = new Form(new Window("Stilla","window4.jsp"));
-                            form3.add(new SubmitButton(new Image("/pics/arachnea/edit.gif")));
-                            form3.add(new Parameter("ib_object_instance_id",""+instances[i].getID()));*/
-                            AdminButton form4 = new AdminButton(new Image("/common/pics/arachnea/edit.gif"),window);
-                            form4.addParameter("ib_window_action","window4");
-                            form4.addParameter(new Parameter("ib_object_instance_id",""+instances[i].getID()));
-
-                            toolbarTable.add(form4,3,1);
-                            super.add(moduleTable);
-                          }
-                  }
-          }
-          catch(Exception ex){
-                  add("villa 1");
-                  ex.printStackTrace(modinfo.getResponse().getWriter());
-          }
-
-          //AdminButton form = new AdminButton("Bæta við",window);
-          if(isAdmin){
-            AdminButton form = new AdminButton(new Image("/common/pics/arachnea/add.gif"),window);
-            form.addParameter("ib_window_action","window1");
-            form.addParameter(new Parameter("page_id",Integer.toString(id)));
-
-            /*Form form = new Form(new Window("Baeta","window1.jsp"));
-            form.add(new SubmitButton("Bæta við"));
-            form.add(new Parameter("page_id",""+id));*/
-
-            add(form);
-          }
-          /*if(getSessionAttribute("idega_entity") == null){
-                  out.print("idega_entity == null");
-          }
-          else{
-                  out.print("idega entity != null");
-          }*/
+      add(form);
+    }
+    /*if(getSessionAttribute("idega_entity") == null){
+            out.print("idega_entity == null");
+    }
+    else{
+            out.print("idega entity != null");
+    }*/
   }
 
 
 
-        /**
-         * Needs reimplementation
-         */
-       public ModuleObject getSetObject(IBObjectInstance instance)throws Exception{
-        return null;
-       }
+  /**
+   * Needs reimplementation
+   */
+  public ModuleObject getSetObject(IBObjectInstance instance) throws Exception {
+    return null;
+  }
 
-       /*
-      public ModuleObject getSetObject(IBObjectInstance instance)throws Exception{
+  /*
+  public ModuleObject getSetObject(IBObjectInstance instance) throws Exception {
 
-              IBObjectProperty[] properties = instance.getProperties();
-              ModuleObject object = null;
+    IBObjectProperty[] properties = instance.getProperties();
+    ModuleObject object = null;
 
-              if (properties.length > 0){
-                      IBObjectPropertyValue[] values;
-                      Class[] classType = new Class[1];
-                      String[] StringArr = new String[1];
-                      classType[0]=Class.forName("java.lang.String");
-                      for(int i=0;i<properties.length;i++){
-                              values = properties[i].getPropertyValues();
-                              if(properties[i].getPropertyType().equals("constructor")){
-                                      //Vector vector;
-                                      Object[] objectArr = new Object[values.length];
-                                      for(int n=0;n<values.length;n++){
-                                              StringArr[0]=values[n].getStringValue();
-                                              objectArr[n]=values[n].getClassObject().getConstructor(classType).newInstance(StringArr);
-
-                                      }
-                                      Class myClass = Class.forName(instance.getObject().getClassName());
-
-                              }
-                              else if(properties[i].getPropertyType().equals("function")){
-                                      if (object==null){
-                                              object=instance.getNewInstance();
-                                      }
-
-                              }
-                              else{
-                                      if (object==null){
-                                              object=instance.getNewInstance();
-                                      }
-                              }
-                      }
-
-
-              }
-              else{
-                      object=instance.getNewInstance();
-              }
-
-              return object;
-      }*/
-
+    if (properties.length > 0) {
+      IBObjectPropertyValue[] values;
+      Class[] classType = new Class[1];
+      String[] StringArr = new String[1];
+      classType[0] = Class.forName("java.lang.String");
+      for(int i = 0; i < properties.length; i++) {
+        values = properties[i].getPropertyValues();
+        if(properties[i].getPropertyType().equals("constructor")){
+          //Vector vector;
+          Object[] objectArr = new Object[values.length];
+          for(int n = 0; n < values.length; n++){
+            StringArr[0] = values[n].getStringValue();
+            objectArr[n] = values[n].getClassObject().getConstructor(classType).newInstance(StringArr);
+          }
+          Class myClass = Class.forName(instance.getObject().getClassName());
+        }
+        else if (properties[i].getPropertyType().equals("function")) {
+          if (object == null){
+            object = instance.getNewInstance();
+          }
+        }
+        else {
+          if (object == null) {
+            object=instance.getNewInstance();
+          }
+        }
+      }
+    }
+    else {
+      object=instance.getNewInstance();
+    }
+    return object;
+  }*/
 }
