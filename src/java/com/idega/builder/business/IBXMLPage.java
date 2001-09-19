8b9c53730c69bc997166f86c6447778db9c579b8
Pall Helgason <palli@idega.com>
2001-09-19 13:36:37 +0000

    no message

--- src/java/com/idega/builder/business/IBXMLPage.java
+++ src/java/com/idega/builder/business/IBXMLPage.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IBXMLPage.java,v 1.5 2001/09/19 01:06:19 palli Exp $
+ * $Id: IBXMLPage.java,v 1.6 2001/09/19 13:36:37 palli Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -24,11 +24,8 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 import com.idega.builder.data.IBPage;
-
 import com.idega.exception.PageDoesNotExist;
-
 import com.idega.jmodule.object.Page;
-
 
 /**
  * A class that reads XML page descriptions from the database and returns
@@ -39,77 +36,85 @@ import com.idega.jmodule.object.Page;
  */
 
 public class IBXMLPage {
-  private SAXBuilder builder = null;
-  private Document xmlDocument = null;
-  private Element rootElement = null;
-  private Page populatedPage = null;
-  private String key;
+  private final static String EMPTY = "";
+  private SAXBuilder _builder = null;
+  private Document _xmlDocument = null;
+  private Element _rootElement = null;
+  private Page _populatedPage = null;
+  private String _key;
 
   /**
-   * @todo Verð að gera þetta öðru vísi (á morgun).
+   * @todo Verð að gera þetta öðru vísi
    */
   private java.util.Hashtable _children = null;
 
-
+  /*
+   *
+   */
   private IBXMLPage(boolean verify) {
-    builder = new SAXBuilder(verify);
+    _builder = new SAXBuilder(verify);
   }
 
-  /*public IBXMLPage(boolean verify,InputStream stream)throws PageDoesNotExist{
-    this(verify);
-    setXMLPageDescriptionFile(stream);
-  }*/
-
+  /**
+   *
+   */
   public IBXMLPage(boolean verify, String key) {
     this(verify);
-    this.key = key;
+    _key = key;
 
     IBPage ibpage = null;
-      try{
-        ibpage = new IBPage(Integer.parseInt(key));
-        setXMLPageDescriptionFile(ibpage.getPageValue());
+    try {
+      ibpage = new IBPage(Integer.parseInt(key));
+      setXMLPageDescriptionFile(ibpage.getPageValue());
+    }
+    catch(PageDoesNotExist pe) {
+      int template = ibpage.getTemplateId();
+      String templateString = null;
+      if (template != -1)
+        templateString = Integer.toString(template);
+      if (ibpage.getType().equals(ibpage.PAGE))
+        setPageAsEmptyPage(XMLConstants.PAGE_TYPE_PAGE,templateString);
+      if (ibpage.getType().equals(ibpage.DRAFT))
+        setPageAsEmptyPage(XMLConstants.PAGE_TYPE_DRAFT,templateString);
+      if (ibpage.getType().equals(ibpage.TEMPLATE))
+        setPageAsEmptyPage(XMLConstants.PAGE_TYPE_TEMPLATE,templateString);
+      else
+        setPageAsEmptyPage(XMLConstants.PAGE_TYPE_PAGE,templateString);
+    }
+    catch(NumberFormatException ne) {
+      try {
+        InputStream stream = new FileInputStream(key);
+        setXMLPageDescriptionFile(stream);
+      }
+      catch(FileNotFoundException fnfe) {
+        fnfe.printStackTrace();
       }
       catch(PageDoesNotExist pe) {
-        int template = ibpage.getTemplateId();
-        String templateString = null;
-        if (template != -1)
-          templateString = Integer.toString(template);
-        if (ibpage.getType().equals(ibpage.PAGE))
-          setPageAsEmptyPage(XMLConstants.PAGE_TYPE_PAGE,templateString);
-        if (ibpage.getType().equals(ibpage.DRAFT))
-          setPageAsEmptyPage(XMLConstants.PAGE_TYPE_DRAFT,templateString);
-        if (ibpage.getType().equals(ibpage.TEMPLATE))
-          setPageAsEmptyPage(XMLConstants.PAGE_TYPE_TEMPLATE,templateString);
-        else
-          setPageAsEmptyPage(XMLConstants.PAGE_TYPE_PAGE,templateString);
+        setPageAsEmptyPage(null,null);
       }
-      catch(NumberFormatException ne) {
-        try{
-          InputStream stream = new FileInputStream(key);
-          setXMLPageDescriptionFile(stream);
-        }
-        catch(FileNotFoundException fnfe){
-          fnfe.printStackTrace();
-        }
-        catch(PageDoesNotExist pe){
-          setPageAsEmptyPage(null,null);
-        }
-      }
-      catch(Exception e){
-        e.printStackTrace();
-      }
-      setPopulatedPage(XMLReader.getPopulatedPage(this));
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+    setPopulatedPage(XMLReader.getPopulatedPage(this));
   }
 
+  /**
+   *
+   */
   public void addChild(String id) {
     if (_children == null)
       _children = new java.util.Hashtable();
 
-    _children.put(id,null);
+    _children.put(id,EMPTY);
   }
 
+  /**
+   *
+   */
   public java.util.Map getChildren() {
-System.out.println("Getting children");
+  //Skítamix
+    _children = null;
     if (_children == null)
       findAllChildren();
     return(_children);
@@ -118,52 +123,57 @@ System.out.println("Getting children");
   private void findAllChildren() {
     _children = new java.util.Hashtable();
     try {
-      String templateId = key;
+      String templateId = _key;
       java.util.List l = com.idega.data.EntityFinder.findAllByColumn(new com.idega.builder.data.IBPage(),com.idega.builder.data.IBPage.getColumnTemplateID(),Integer.parseInt(templateId));
+      if (l == null)
+        return;
       java.util.Iterator i = l.iterator();
       while (i.hasNext()) {
         com.idega.builder.data.IBPage p = (com.idega.builder.data.IBPage)i.next();
-        System.out.println("id = " + p.getID());
-        _children.put(Integer.toString(p.getID()),new String(""));
+        _children.put(Integer.toString(p.getID()),EMPTY);
       }
     }
     catch(java.sql.SQLException e) {}
   }
 
 
-  public boolean update(){
+  public boolean update() {
+    try {
+      System.out.println("starting update");
+      IBPage ibpage = new IBPage(Integer.parseInt(_key));
+      OutputStream stream = ibpage.getPageValueForWrite();
+      store(stream);
+      ibpage.update();
+      System.out.println("Getting children");
+      java.util.Hashtable h = (java.util.Hashtable)getChildren();
+      if (h != null) {
+        System.out.println("Got children hashtable");
+        java.util.Enumeration e = h.keys();
+        while (e.hasMoreElements()) {
+          String invalid = (String)e.nextElement();
+          System.out.println("invalidating page " + invalid);
+          PageCacher.flagPageInvalid(invalid);
+        }
+      }
+      //ibpage.setPageValue(xmlDocument.);
+      //setXMLPageDescriptionFile(ibpage.getPageValue());
+    }
+    catch(NumberFormatException ne){
       try{
-        IBPage ibpage = new IBPage(Integer.parseInt(key));
-        OutputStream stream = ibpage.getPageValueForWrite();
+        //InputStream stream = new FileInputStream(key);
+        //setXMLPageDescriptionFile(stream);
+        OutputStream stream = new FileOutputStream(_key);
         store(stream);
-        ibpage.update();
-System.out.println("Getting children!!!");
-        java.util.Hashtable h = (java.util.Hashtable)getChildren();
-        if (h != null) {
-          java.util.Enumeration e = h.keys();
-          while (e.hasMoreElements()) {
-            PageCacher.flagPageInvalid((String)e.nextElement());
-          }
-        }
-        //ibpage.setPageValue(xmlDocument.);
-        //setXMLPageDescriptionFile(ibpage.getPageValue());
       }
-      catch(NumberFormatException ne){
-        try{
-          //InputStream stream = new FileInputStream(key);
-          //setXMLPageDescriptionFile(stream);
-          OutputStream stream = new FileOutputStream(key);
-          store(stream);
-        }
-        catch(FileNotFoundException fnfe){
-          fnfe.printStackTrace();
-        }
+      catch(FileNotFoundException fnfe){
+        fnfe.printStackTrace();
       }
-      catch(Exception e){
-        e.printStackTrace();
-      }
-      setPopulatedPage(XMLReader.getPopulatedPage(this));
-      return true;
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+    setPopulatedPage(XMLReader.getPopulatedPage(this));
+    return true;
   }
 
   private void store(OutputStream stream) {
@@ -171,7 +181,7 @@ System.out.println("Getting children!!!");
       XMLOutputter outputter = new XMLOutputter("  ",true);
       outputter.setLineSeparator(System.getProperty("line.separator"));
       outputter.setTrimText(true);
-      outputter.output(xmlDocument,stream);
+      outputter.output(_xmlDocument,stream);
       stream.close();
     }
     catch(IOException e) {
@@ -180,11 +190,11 @@ System.out.println("Getting children!!!");
   }
 
   public void setPopulatedPage(Page page){
-    populatedPage=page;
+    _populatedPage=page;
   }
 
   public Page getPopulatedPage(){
-    return populatedPage;
+    return _populatedPage;
   }
 
   /**
@@ -196,8 +206,8 @@ System.out.println("Getting children!!!");
    */
   public void setXMLPageDescriptionFile(String URI) throws PageDoesNotExist {
     try {
-      xmlDocument = builder.build(URI);
-      rootElement = xmlDocument.getRootElement();
+      _xmlDocument = _builder.build(URI);
+      _rootElement = _xmlDocument.getRootElement();
     }
     catch(org.jdom.JDOMException e) {
       System.err.println("JDOM Exception: " + e.getMessage());
@@ -217,9 +227,9 @@ System.out.println("Getting children!!!");
   public void setXMLPageDescriptionFile(InputStream stream) throws PageDoesNotExist {
     boolean streamopen=true;
     try {
-      xmlDocument = builder.build(stream);
+      _xmlDocument = _builder.build(stream);
       stream.close();
-      rootElement = xmlDocument.getRootElement();
+      _rootElement = _xmlDocument.getRootElement();
       streamopen=false;
     }
     catch(org.jdom.JDOMException e) {
@@ -246,7 +256,7 @@ System.out.println("Getting children!!!");
 
   public void setPageAsEmptyPage(String type, String template) {
     System.out.println("setPageAsEmptyPage");
-    rootElement = new Element(XMLConstants.ROOT_STRING);
+    _rootElement = new Element(XMLConstants.ROOT_STRING);
     Element pageElement = new Element(XMLConstants.PAGE_STRING);
 
     if (type == null)
@@ -262,9 +272,8 @@ System.out.println("Getting children!!!");
     if (template != null)
       pageElement.addAttribute(XMLConstants.TEMPLATE_STRING,template);
 
-
-    rootElement.addContent(pageElement);
-    xmlDocument = new Document(rootElement);
+    _rootElement.addContent(pageElement);
+    _xmlDocument = new Document(_rootElement);
     setPopulatedPage(XMLReader.getPopulatedPage(this));
   }
 
@@ -275,12 +284,12 @@ System.out.println("Getting children!!!");
    * @todo Wrap the Element class to hide all implementation of the XML parser.
    */
   Element getRootElement() {
-    return(rootElement);
+    return(_rootElement);
   }
 
   Element getPageRootElement(){
-    if(rootElement!=null){
-      return rootElement.getChild(XMLConstants.PAGE_STRING);
+    if(_rootElement!=null){
+      return _rootElement.getChild(XMLConstants.PAGE_STRING);
     }
     return null;
   }
