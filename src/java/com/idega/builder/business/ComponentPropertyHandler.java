d31d1212d343d4b71aac420e2027d001daeeff14
Eirikur Hrafnsson <eiki@idega.com>
2002-09-27 13:38:35 +0000

    *** empty log message ***

--- src/java/com/idega/builder/business/ComponentPropertyHandler.java
+++ src/java/com/idega/builder/business/ComponentPropertyHandler.java
@@ -157,7 +157,7 @@ public class ComponentPropertyHandler {
         //REMOVE AND MAKE GENERIC! ask tryggvi and eiki
         else if(parameterType.equals(Group.class)){
           try {
-            argument = (Group) ((GroupHome)com.idega.data.IDOLookup.getHome(Group.class)).findByPrimaryKey(new Integer(stringValue));
+            argument = (Group) ((GroupHome)com.idega.data.IDOLookup.getHome(Group.class)).findByPrimaryKey(new Integer( stringValue.substring(stringValue.lastIndexOf('-')+1, stringValue.length()) ));
           }
           catch (Exception ex) {
             ex.printStackTrace(System.err);
