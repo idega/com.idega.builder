7d7397b2be28692eb28b972e9f24c94832afe142
Thomas Hilbig <thomas@idega.com>
2003-02-03 12:14:44 +0000

    first version

---
+++ src/java/com/idega/builder/handler/SpecifiedChoiceProvider.java
@@ -0,0 +1,27 @@
+package com.idega.builder.handler;
+
+
+import java.util.Collection;
+
+import com.idega.builder.business.IBPropertyHandler;
+import com.idega.presentation.IWContext;
+
+/**
+ *@author     <a href="mailto:thomas@idega.is">Thomas Hilbig</a>
+ *@version    1.0
+ *
+ *	Presentation objects that want to use the DropDownMenuSpecifiedChoiceHandler
+ *	must implement this interface.
+ *	The	 method below is used by the IBPropertyHandler to get a collection of
+ *	strings							 from	 the presentation object. The collection is
+ *	finally	 set into the drop 	down	 menu of the handler.
+ *
+ */
+public interface SpecifiedChoiceProvider {
+  
+  public Collection getSpecifiedChoice(
+    IWContext iwc, 
+    String ICObjectInstanceID, 
+    String methodIdentifier, 
+    IBPropertyHandler propertyHandler);
+}
