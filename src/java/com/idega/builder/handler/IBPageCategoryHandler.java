699ebbd59fd6f405b23d3b2661702281279e4ab0
Thorhallur Helgason <laddi@idega.com>
2003-09-18 11:42:29 +0000

    Handler to use to set IBPage as a category page.

---
+++ src/java/com/idega/builder/handler/IBPageCategoryHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Created on 17.9.2003
+ */
+package com.idega.builder.handler;
+
+import java.util.List;
+import java.util.Map;
+
+import com.idega.builder.business.BuilderLogic;
+import com.idega.builder.business.IBPageUpdater;
+import com.idega.builder.business.PageTreeNode;
+import com.idega.core.localisation.business.ICLocaleBusiness;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.BooleanInput;
+
+/**
+ * @author laddi
+ */
+public class IBPageCategoryHandler implements PropertyHandler {
+
+	/* (non-Javadoc)
+	 * @see com.idega.builder.handler.PropertyHandler#getDefaultHandlerTypes()
+	 */
+	public List getDefaultHandlerTypes() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.builder.handler.PropertyHandler#getHandlerObject(java.lang.String, java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		BooleanInput input = new BooleanInput(name);
+		input.setSelectedElement(stringValue);
+		
+		return input;
+	}
+
+	public void onUpdate(String[] values, IWContext iwc) {
+		if (values != null) {
+			String value = values[0];
+			boolean isCategory = false;
+			if (value.equalsIgnoreCase("Y"))
+				isCategory = true;
+			
+			int currentPage = BuilderLogic.getInstance().getCurrentIBPageID(iwc);
+			if (currentPage != -1) {
+				IBPageUpdater.setAsCategory(currentPage, isCategory);
+			}
+		}		
+	}
+}
