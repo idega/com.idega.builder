32029efa86f289f3b7d4dd46812599331044e12d
Thomas Hilbig <thomas@idega.com>
2004-03-26 17:31:36 +0000

    new methods added

--- src/java/com/idega/builder/business/FileBusiness.java
+++ src/java/com/idega/builder/business/FileBusiness.java
@@ -3,6 +3,9 @@ package com.idega.builder.business;
 
 public interface FileBusiness extends com.idega.business.IBOService,com.idega.io.ObjectWriter
 {
+ public com.idega.builder.data.IBExportImportData getIBExportImportData(com.idega.io.UploadFile p0)throws java.io.IOException, java.rmi.RemoteException;
  public java.lang.String getURLForOfferingDownload(com.idega.io.Storable p0)throws java.io.IOException, java.rmi.RemoteException;
- public java.lang.String getURLForOfferingDownload(java.lang.String p0,java.util.List p1,com.idega.builder.data.IBExportMetadata p2)throws java.io.IOException, java.rmi.RemoteException;
+ public java.lang.Object write(com.idega.core.file.data.ICFile p0) throws java.rmi.RemoteException;
+ public java.lang.Object write(com.idega.builder.data.IBExportImportData p0) throws java.rmi.RemoteException;
+ public java.lang.Object write(com.idega.util.xml.XMLData p0) throws java.rmi.RemoteException;
 }
--- src/java/com/idega/builder/business/FileBusinessBean.java
+++ src/java/com/idega/builder/business/FileBusinessBean.java
@@ -1,23 +1,18 @@
 package com.idega.builder.business;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
-import com.idega.builder.data.IBExportMetadata;
+import com.idega.builder.data.IBExportImportData;
 import com.idega.business.IBOServiceBean;
 import com.idega.core.file.data.ICFile;
 import com.idega.io.ICFileWriter;
 import com.idega.io.ObjectWriter;
 import com.idega.io.Storable;
+import com.idega.io.UploadFile;
+import com.idega.io.WriterToFile;
 import com.idega.io.XMLDataWriter;
+import com.idega.io.IBExportImportDataWriter;
+import com.idega.io.IBExportImportDataReader;
 import com.idega.util.xml.XMLData;
 
 /**
@@ -30,78 +25,19 @@ import com.idega.util.xml.XMLData;
  * Created on Mar 15, 2004
  */
 public class FileBusinessBean extends IBOServiceBean  implements  FileBusiness,ObjectWriter {
-
-	private static final String ZIP_EXTENSION = "zip";	
-	private static final String ZIP_ELEMENT_DELIMITER = "_";
 	
-
-	
+	public IBExportImportData getIBExportImportData(UploadFile uploadFile) throws IOException {
+		IBExportImportDataReader exportImportDataReader = new IBExportImportDataReader ();
+		return exportImportDataReader.getData(uploadFile);
+	}
+		
 	public String getURLForOfferingDownload(Storable storableObject) throws IOException {
 		return createContainer(storableObject);
 	}
 	
 	private String createContainer(Storable storableObject) throws IOException {
-		ICFileWriter currentWriter = (ICFileWriter) storableObject.write(this);
+		WriterToFile currentWriter = (WriterToFile) storableObject.write(this);
 		return currentWriter.createContainer(); 
-	}
-	
-	public String getURLForOfferingDownload(String name, List data, IBExportMetadata metadata) throws IOException {
-		return createContainer(name, data, metadata);
-	}
-	
-	private String createContainer(String name, Collection data, IBExportMetadata metadata) throws IOException {
-		ICFileWriter currentWriter = new ICFileWriter(getIWApplicationContext());
-		long folderIdentifier = System.currentTimeMillis();
- 		String path = currentWriter.getRealPathToFile(name, ZIP_EXTENSION, folderIdentifier);
-     File auxiliaryFile = null;
-     ZipOutputStream destination = null;
-     try {
-       auxiliaryFile = new File(path);
-       destination = new ZipOutputStream(new FileOutputStream(auxiliaryFile));
-     }
-     catch (FileNotFoundException ex)  {
-//     	logError("[XMLData] problem creating file.");
-//     	log(ex);
-     	throw new IOException("xml file could not be stored");
-     }
-     writeData(data, metadata, destination);
-     currentWriter.close(destination);
-     return currentWriter.getURLToFile(name, ZIP_EXTENSION, folderIdentifier);
- 	}
-	
-	private void writeData(Collection data, IBExportMetadata metadata, ZipOutputStream destination) throws IOException {
-		List alreadyStoredElements = new ArrayList(data.size());
-		// counter for the entries in metadata
-		int entryNumber = 0;
-		// counter for the prefix of the stored files
-		int identifierNumber = 0;
- 		Iterator iterator = data.iterator();
- 		while (iterator.hasNext()) {
- 			Storable element = (Storable) iterator.next();
- 			// do not store the same elements twice into the zip file
- 			int indexOfAlreadyStoredElement;
- 			if ((indexOfAlreadyStoredElement = alreadyStoredElements.indexOf(element)) > - 1) {
- 				metadata.modifyElementSetNameSetOriginalNameLikeElementAt(entryNumber++, indexOfAlreadyStoredElement);
- 			}
- 			else {
-	 			alreadyStoredElements.add(element);
-	 			ICFileWriter currentWriter = (ICFileWriter) element.write(this);
-	 			String originalName = currentWriter.getName();
-	 			String zipElementName = createZipElementName(originalName, identifierNumber++);
-	 			ZipEntry zipEntry = new ZipEntry(zipElementName);
-	 			metadata.modifyElementSetNameSetOriginalName(entryNumber++, zipElementName, originalName);
-	 			destination.putNextEntry(zipEntry);
-	 			currentWriter.writeData(destination);
-	 			destination.closeEntry();
- 			}
- 		}
- 		// add metadata itself to the zip file
- 		ICFileWriter currentWriter = (ICFileWriter) metadata.write(this);
-		String originalName = currentWriter.getName();
-		ZipEntry zipEntry = new ZipEntry(originalName);
-		destination.putNextEntry(zipEntry);
-		currentWriter.writeData(destination);
-		destination.closeEntry();
 	}
 	
   public Object write(ICFile file) {
@@ -112,11 +48,9 @@ public class FileBusinessBean extends IBOServiceBean  implements  FileBusiness,O
 		return new XMLDataWriter(xmlData, getIWApplicationContext());
 	}
   
-  private String createZipElementName(String originalName, int entryNumber) {
-  	StringBuffer buffer = new StringBuffer();
-  	buffer.append(entryNumber).append(ZIP_ELEMENT_DELIMITER).append(originalName);
-  	return buffer.toString();
+  public Object write(IBExportImportData metadata) {
+  	return new IBExportImportDataWriter(metadata, getIWApplicationContext());
   }
-  	
+  
 
 }  	
