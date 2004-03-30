bbdbc11f436de282bb6275ec42c5dd6df9ef7df8
Thomas Hilbig <thomas@idega.com>
2004-03-30 15:51:09 +0000

    improved versions

--- src/java/com/idega/builder/data/IBExportImportData.java
+++ src/java/com/idega/builder/data/IBExportImportData.java
@@ -33,10 +33,23 @@ public class IBExportImportData implements Storable {
 	public static final String EXPORT_METADATA_FILE_NAME = EXPORT_METADATA_NAME + ".xml";
 	
 	private List files = new ArrayList();
-	private List fileElements = new ArrayList();
+	private List fileElements = new ArrayList(); 
 	private XMLElement pagesElement = null; 
 	private XMLElement templatesElement = null;
+	private XMLData metadataSummary = null;
 
+	public static IBExportImportData getInstance(XMLData metadataSummary) {
+		IBExportImportData exportImportMetadata = new IBExportImportData();
+		exportImportMetadata.metadataSummary = metadataSummary;
+		exportImportMetadata.setFilesElement();
+		return exportImportMetadata;
+	}
+	
+	private static String getSourceClassForPage() {
+		 return ICPage.class.getName();
+	}
+		
+	
 	public String getName() {
 		return EXPORT_METADATA_NAME;
 	}
@@ -45,22 +58,35 @@ public class IBExportImportData implements Storable {
 		return files;
 	}
 	
-	public String getSourceClassNameForElement(int index) {
-		XMLElement fileElement = (XMLElement) fileElements.get(index);
-		return fileElement.getTextTrim(XMLConstants.FILE_SOURCE);
+	public String getMimeType(String usedId) {
+		XMLElement fileElement;
+		if ((fileElement = findFileElementByUsedId(usedId)) != null) { 
+			return fileElement.getTextTrim(XMLConstants.FILE_MIME_TYPE);
+		}
+		return null;
 	}
 	
-	public void modifyElementSetNameSetOriginalName(int index, String name, String originalName) {
+	public boolean isFileEntryAPage(String usedId) {
+		XMLElement fileElement;
+		if ((fileElement = findFileElementByUsedId(usedId)) != null) { 
+			return IBExportImportData.getSourceClassForPage().equals(fileElement.getTextTrim(XMLConstants.FILE_SOURCE));
+		}
+		return false;
+	}
+	
+	public void modifyElementSetNameSetOriginalName(int index, String name, String originalName, String mimeType) {
 		XMLElement fileElement = (XMLElement) fileElements.get(index);
 		fileElement.addContent(XMLConstants.FILE_USED_ID,name);
 		fileElement.addContent(XMLConstants.FILE_ORIGINAL_NAME, originalName);
+		fileElement.addContent(XMLConstants.FILE_MIME_TYPE, mimeType);
 	}
 	
 	public void modifyElementSetNameSetOriginalNameLikeElementAt(int index, int existingDataIndex) {
 		XMLElement fileElement = (XMLElement) fileElements.get(existingDataIndex);
 		String name = fileElement.getTextTrim(XMLConstants.FILE_USED_ID);
 		String originalName = fileElement.getTextTrim(XMLConstants.FILE_ORIGINAL_NAME);
-		modifyElementSetNameSetOriginalName(index, name, originalName);
+		String mimeType = fileElement.getTextTrim(XMLConstants.FILE_MIME_TYPE);
+		modifyElementSetNameSetOriginalName(index, name, originalName, mimeType);
 	}
 		
 	public void addPageTree(IWContext iwc) throws IDOLookupException, FinderException {
@@ -95,7 +121,7 @@ public class IBExportImportData implements Storable {
 	
 	public void addFileEntry(IBReference.Entry entry, Storable storable, String value) {
 		files.add(storable);
-		XMLElement fileElement = new XMLElement(XMLConstants.FILE);
+		XMLElement fileElement = new XMLElement(XMLConstants.FILE_FILE);
 		fileElement.addContent(XMLConstants.FILE_SOURCE, entry.getSourceClass());
 		fileElement.addContent(XMLConstants.FILE_NAME, entry.getValueName());
 		fileElement.addContent(XMLConstants.FILE_VALUE, value);
@@ -104,8 +130,8 @@ public class IBExportImportData implements Storable {
 	
 	public void addFileEntry(ICPage page) {
 		files.add(page);
-		XMLElement fileElement = new XMLElement(XMLConstants.FILE);
-		fileElement.addContent(XMLConstants.FILE_SOURCE, ICPage.class.getName());
+		XMLElement fileElement = new XMLElement(XMLConstants.FILE_FILE);
+		fileElement.addContent(XMLConstants.FILE_SOURCE, IBExportImportData.getSourceClassForPage());
 		fileElement.addContent(XMLConstants.FILE_NAME, page.getIDColumnName());
 		fileElement.addContent(XMLConstants.FILE_VALUE, page.getPrimaryKey().toString());
 		fileElements.add(fileElement);
@@ -118,16 +144,39 @@ public class IBExportImportData implements Storable {
 	public XMLData createMetadataSummary() {
 		XMLData metadata = XMLData.getInstanceWithoutExistingFileSetNameSetRootName(EXPORT_METADATA_FILE_NAME, EXPORT_METADATA_NAME);
 		XMLElement metadataElement = metadata.getDocument().getRootElement();
+		XMLElement filesElement = new XMLElement(XMLConstants.FILE_FILES);
+		metadataElement.addContent(filesElement);
 		Iterator iterator = fileElements.iterator();
 		while (iterator.hasNext()) {
 			XMLElement fileElement = (XMLElement) iterator.next();
-			metadataElement.addContent(fileElement);
+			filesElement.addContent(fileElement);
 		}
 		metadataElement.addContent(pagesElement);
 		metadataElement.addContent(templatesElement);
 		return metadata;
 	}
+	
+	private void setFilesElement() {
+		XMLElement filesElement = metadataSummary.getDocument().getRootElement().getChild(XMLConstants.FILE_FILES);
+		fileElements =  filesElement.getChildren();
+	}
+			
+	private XMLElement findFileElementByUsedId(String usedId) {
+	Iterator fileElementIterator = fileElements.iterator();
+		while (fileElementIterator.hasNext()) {
+			XMLElement fileElement = (XMLElement) fileElementIterator.next();
+			String currentName = fileElement.getTextTrim(XMLConstants.FILE_USED_ID);
+			if (currentName != null && currentName.equals(usedId)) {
+				return fileElement;
+			}
+		}
+		return null;
+	}	
 
 }
+		
+	
+
+
 
 
