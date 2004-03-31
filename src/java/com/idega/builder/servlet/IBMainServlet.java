450406f4beb4046e95edf2ba73124e3f51710d3a
Eirikur Hrafnsson <eiki@idega.com>
2004-03-31 17:22:28 +0000

    Fixed synhronization bug (deadlock) by overriding the method:protected boolean getIfSyncronizeAccess(ServletRequest _req,ServletResponse _res) {  Because if using a PageIncluder that calls a page on the same server and the pageincluder page is the first page loaded  we will get a deadlock in the super impl of this method.  However one will never create a pageincluder pointing to the page it is contained in so all we need to do is to  keep track of what ibpages have been initilized and only synchronize a page when it is first being loaded and not lock out other  pages at the same time.

--- src/java/com/idega/builder/servlet/IBMainServlet.java
+++ src/java/com/idega/builder/servlet/IBMainServlet.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IBMainServlet.java,v 1.24 2003/08/06 09:45:09 laddi Exp $
+ * $Id: IBMainServlet.java,v 1.25 2004/03/31 17:22:28 eiki Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -9,6 +9,13 @@
  */
 package com.idega.builder.servlet;
 
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 import com.idega.builder.business.BuilderLogic;
 import com.idega.core.builder.business.BuilderService;
@@ -24,6 +31,9 @@ import com.idega.servlet.IWJSPPresentationServlet;
 
 public class IBMainServlet extends IWJSPPresentationServlet {
 
+    //needs to be static because there are at least 2 instances of IBMainServlet , /servlet/IBMainServlet and index.jsp
+  private static List initializedPageIDs;
+      
   public void initializePage()throws Exception{
   	setHandleJSPTags(false);
   	IWApplicationContext iwac = this.getApplication().getIWApplicationContext();
@@ -45,4 +55,41 @@ public class IBMainServlet extends IWJSPPresentationServlet {
   }
   
 
+    /* (non-Javadoc)
+     * @see com.idega.servlet.IWCoreServlet#getIfSyncronizeAccess(javax.servlet.ServletRequest, javax.servlet.ServletResponse)
+     */
+    protected boolean getIfSyncronizeAccess(ServletRequest _req,ServletResponse _res) {
+        //if using a PageIncluder that calls a page on the same server and the pageincluder page is the first page loaded
+        //we will get a deadlock in the super impl of this method. 
+        //However one will never create a pageincluder pointing to the page it is contained in so all we need to do is to
+        //keep track of what ibpages have been initilized and only synchronize a page when it is first being loaded and not lock out other
+        //pages at the same time.
+        //eiki@idega.is
+        if(initializedPageIDs==null) {
+            initializedPageIDs = new ArrayList();
+        }
+        
+        try {
+            IWApplicationContext iwac = this.getApplication().getIWApplicationContext();
+            BuilderService bs = BuilderServiceFactory.getBuilderService(iwac);
+	        IWContext iwc = getIWContext();
+
+	        Integer pageID = new Integer(bs.getCurrentPageId(iwc));
+	        if(initializedPageIDs.contains(pageID)) {
+	            //no need to synchronize
+	            return false;
+	        }
+	        else {
+	            //synchronize but add the id to memory so we don't do it again
+	            initializedPageIDs.add(pageID);
+	            return true;
+	        }
+	        
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        
+        //if all else failes, use the default
+        return super.getIfSyncronizeAccess(_req, _res);
+    }
 }
