c61e0afa4930081a455eddbae2071e68a428b245
Thomas Hilbig <thomas@idega.com>
2004-06-16 16:43:23 +0000

    avoiding direct references by using repository

--- src/java/com/idega/builder/IWBundleStarter.java
+++ src/java/com/idega/builder/IWBundleStarter.java
@@ -1,7 +1,12 @@
 package com.idega.builder;
 
+import com.idega.builder.presentation.InvisibleInBuilder;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
+import com.idega.presentation.Applet;
+import com.idega.presentation.GenericPlugin;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.repository.data.ImplementorRepository;
 
 /**
  * <p>Title: idegaWeb</p>
@@ -15,6 +20,10 @@ import com.idega.idegaweb.IWBundleStartable;
 public class IWBundleStarter implements IWBundleStartable {
 
 	public void start(IWBundle starterBundle) {
+		ImplementorRepository repository = ImplementorRepository.getInstance();
+		repository.addImplementor(InvisibleInBuilder.class, Applet.class);
+		repository.addImplementor(InvisibleInBuilder.class, GenericPlugin.class);
+		repository.addImplementor(InvisibleInBuilder.class, DropdownMenu.class);
 	}
 
 	
