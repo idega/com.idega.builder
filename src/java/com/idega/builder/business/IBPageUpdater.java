ef0719f1dc18baab384dcc8763a654b8e84572dc
Valdas Zemaitis <valdas@idega.com>
2008-01-12 13:27:40 +0000

    Removed needless print of error message

--- src/java/com/idega/builder/business/IBPageUpdater.java
+++ src/java/com/idega/builder/business/IBPageUpdater.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IBPageUpdater.java,v 1.8 2008/01/11 15:12:03 valdas Exp $
+ * $Id: IBPageUpdater.java,v 1.9 2008/01/12 13:27:40 valdas Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -13,7 +13,6 @@ import java.rmi.RemoteException;
 import java.sql.SQLException;
 
 import javax.ejb.CreateException;
-import javax.ejb.FinderException;
 
 import com.idega.builder.data.IBPageName;
 import com.idega.core.builder.data.ICPage;
@@ -73,7 +72,6 @@ public class IBPageUpdater {
 		try {
 			name = ((com.idega.builder.data.IBPageNameHome) com.idega.data.IDOLookup.getHome(IBPageName.class)).findByPageIdAndLocaleId(pageId, localeId);
 		} catch(Exception e) {
-			e.printStackTrace();
 		}
 		
 		try {
