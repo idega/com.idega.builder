b25f3ec3456ea14e626f721dd742bbad0703ed37
Vitautas Civilis <civilis@idega.com>
2008-04-22 00:13:02 +0000

    spring bean managed by annotations

--- src/java/com/idega/builder/business/BuilderLogicWrapperBean.java
+++ src/java/com/idega/builder/business/BuilderLogicWrapperBean.java
@@ -1,9 +1,14 @@
 package com.idega.builder.business;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
 import com.idega.core.builder.business.BuilderService;
 import com.idega.core.builder.business.BuilderServiceFactory;
 import com.idega.idegaweb.IWApplicationContext;
 
+@Scope("singleton")
+@Service("builderLogicWrapperBeanId")
 public class BuilderLogicWrapperBean implements BuilderLogicWrapper {
 
 	public boolean reloadGroupsInCachedDomain(IWApplicationContext iwac, String serverName) {
