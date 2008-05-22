9efcdd15a15b601c6aab182f38310a4a1c0c1a04
Valdas Zemaitis <valdas@idega.com>
2008-05-22 08:09:13 +0000

    *** empty log message ***

--- src/java/com/idega/builder/business/BuilderLogic.java
+++ src/java/com/idega/builder/business/BuilderLogic.java
@@ -1,5 +1,5 @@
 /*
- * $Id: BuilderLogic.java,v 1.329 2008/05/19 20:25:33 civilis Exp $ Copyright
+ * $Id: BuilderLogic.java,v 1.330 2008/05/22 08:09:13 valdas Exp $ Copyright
  * (C) 2001 Idega hf. All Rights Reserved. This software is the proprietary
  * information of Idega hf. Use is subject to license terms.
  */
@@ -275,7 +275,7 @@ public class BuilderLogic implements Singleton {
 		Web2Business web2 = getWeb2Business(iwc);
 		
 		//	JavaScript
-		adder.addJavaScriptAtPosition(iwc, AddResource.HEADER_BEGIN, "/dwr/util.js");
+		adder.addJavaScriptAtPosition(iwc, AddResource.HEADER_BEGIN, CoreConstants.DWR_UTIL_SCRIPT);
 		adder.addJavaScriptAtPosition(iwc, AddResource.HEADER_BEGIN, "/dwr/interface/BuilderEngine.js");
 		adder.addJavaScriptAtPosition(iwc, AddResource.HEADER_BEGIN, iwb.getVirtualPathWithFileNameString("javascript/builder_general.js"));
 		adder.addJavaScriptAtPosition(iwc, AddResource.HEADER_BEGIN, iwb.getVirtualPathWithFileNameString("javascript/BuilderHelper.js"));
