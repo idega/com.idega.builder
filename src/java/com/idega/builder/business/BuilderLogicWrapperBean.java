a74b03cdbdb936026e6852f375aef40e13fe3228
Valdas Zemaitis <valdas@idega.com>
2008-11-13 07:12:40 +0000

    Moved constants to more proper place

--- src/java/com/idega/builder/business/BuilderLogicWrapperBean.java
+++ src/java/com/idega/builder/business/BuilderLogicWrapperBean.java
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Service;
 import com.idega.core.builder.business.BuilderService;
 import com.idega.core.builder.business.BuilderServiceFactory;
 import com.idega.idegaweb.IWApplicationContext;
-import com.idega.util.CoreConstants;
 
 @Scope("singleton")
-@Service(CoreConstants.SPRING_BEAN_NAME_BUILDER_LOGIC_WRAPPER)
+@Service(BuilderLogicWrapper.SPRING_BEAN_NAME_BUILDER_LOGIC_WRAPPER)
 public class BuilderLogicWrapperBean implements BuilderLogicWrapper {
 
 	public boolean reloadGroupsInCachedDomain(IWApplicationContext iwac, String serverName) {
