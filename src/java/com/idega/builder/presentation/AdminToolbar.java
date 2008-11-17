54306971ddd4986a2f33394b3756f8162eef124b
Thorhallur Helgason <laddi@idega.com>
2008-11-17 22:10:30 +0000

    Layout fixes.

--- src/java/com/idega/builder/presentation/AdminToolbar.java
+++ src/java/com/idega/builder/presentation/AdminToolbar.java
@@ -16,8 +16,10 @@ import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Layer;
 import com.idega.presentation.PresentationObjectTransitional;
+import com.idega.presentation.text.Heading1;
 import com.idega.presentation.text.ListItem;
 import com.idega.presentation.text.Lists;
+import com.idega.presentation.text.Paragraph;
 import com.idega.presentation.text.Text;
 import com.idega.util.CoreConstants;
 import com.idega.util.PresentationUtil;
@@ -28,10 +30,10 @@ import com.idega.util.expression.ELUtil;
  * <p>
  * TODO laddi Describe Type AdminToolbar
  * </p>
- *  Last modified: $Date: 2008/11/17 08:42:42 $ by $Author: laddi $
+ *  Last modified: $Date: 2008/11/17 22:10:30 $ by $Author: laddi $
  * 
  * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class AdminToolbar extends PresentationObjectTransitional {
 
@@ -49,6 +51,8 @@ public class AdminToolbar extends PresentationObjectTransitional {
 		PresentationUtil.addJavaScriptSourceLineToHeader(iwc, CoreConstants.DWR_ENGINE_SCRIPT);
 		PresentationUtil.addJavaScriptSourceLineToHeader(iwc, CoreConstants.DWR_UTIL_SCRIPT);
 		PresentationUtil.addJavaScriptSourceLineToHeader(iwc, "/dwr/interface/AdminToolbarSession.js");
+		PresentationUtil.addJavaScriptSourceLineToHeader(iwc, getBundle(iwc).getVirtualPathWithFileNameString("javascript/jquery.dimensions.js"));
+		PresentationUtil.addJavaScriptSourceLineToHeader(iwc, getBundle(iwc).getVirtualPathWithFileNameString("javascript/jquery.dropshadow.js"));
 		PresentationUtil.addJavaScriptSourceLineToHeader(iwc, getBundle(iwc).getVirtualPathWithFileNameString("javascript/AdminCore.js"));
 		
 		Layer layer = new Layer();
@@ -72,6 +76,21 @@ public class AdminToolbar extends PresentationObjectTransitional {
 		Lists list = new Lists();
 		layer.add(list);
 		
+		ListItem themes = new ListItem();
+		themes.setStyleClass("adminThemesMode");
+		themes.add(new Text(iwrb.getLocalizedString("admin_mode.themes", "Themes")));
+		if (iwc.hasRole(StandardRoles.ROLE_KEY_ADMIN) || iwc.hasRole(StandardRoles.ROLE_KEY_EDITOR)) {
+			list.add(themes);
+		}
+		
+		Layer themesHelp = new Layer();
+		themesHelp.setStyleClass("modeHelper");
+		themesHelp.add(new Heading1(iwrb.getLocalizedString("admin_mode.themes_help", "Themes mode")));
+		Paragraph paragraph = new Paragraph();
+		paragraph.add(new Text(iwrb.getLocalizedString("admin_mode.themes_help_text", "Themes help text")));
+		themesHelp.add(paragraph);
+		themes.add(themesHelp);
+		
 		ListItem edit = new ListItem();
 		edit.setStyleClass("adminEditMode");
 		edit.add(new Text(iwrb.getLocalizedString("admin_mode.edit", "Edit")));
@@ -79,20 +98,46 @@ public class AdminToolbar extends PresentationObjectTransitional {
 			list.add(edit);
 		}
 		
+		Layer editHelp = new Layer();
+		editHelp.setStyleClass("modeHelper");
+		editHelp.add(new Heading1(iwrb.getLocalizedString("admin_mode.edit_help", "Edit mode")));
+		paragraph = new Paragraph();
+		paragraph.add(new Text(iwrb.getLocalizedString("admin_mode.edit_help_text", "Edit help text")));
+		editHelp.add(paragraph);
+		edit.add(editHelp);
+		
 		ListItem content = new ListItem();
 		content.setStyleClass("adminContentMode");
 		content.add(new Text(iwrb.getLocalizedString("admin_mode.content", "Content")));
 		list.add(content);
 		
+		Layer contentHelp = new Layer();
+		contentHelp.setStyleClass("modeHelper");
+		contentHelp.add(new Heading1(iwrb.getLocalizedString("admin_mode.content_help", "Content mode")));
+		paragraph = new Paragraph();
+		paragraph.add(new Text(iwrb.getLocalizedString("admin_mode.content_help_text", "Content help text")));
+		contentHelp.add(paragraph);
+		content.add(contentHelp);
 		ListItem preview = new ListItem();
 		preview.setStyleClass("adminPreviewMode");
 		preview.add(new Text(iwrb.getLocalizedString("admin_mode.preview", "Preview")));
 		list.add(preview);
 		
+		Layer previewHelp = new Layer();
+		previewHelp.setStyleClass("modeHelper");
+		previewHelp.add(new Heading1(iwrb.getLocalizedString("admin_mode.preview_help", "Preview mode")));
+		paragraph = new Paragraph();
+		paragraph.add(new Text(iwrb.getLocalizedString("admin_mode.preview_help_text", "Preview help text")));
+		previewHelp.add(paragraph);
+		preview.add(previewHelp);
+		
 		AdminToolbarSession session = ELUtil.getInstance().getBean(AdminToolbarSession.class);
 		if (session.getMode() != null) {
 			String mode = session.getMode();
-			if (mode.equals("isEditAdmin")) {
+			if (mode.equals("isThemesAdmin")) {
+				themes.setStyleClass("selected");
+			}
+			else if (mode.equals("isEditAdmin")) {
 				edit.setStyleClass("selected");
 			}
 			else if (mode.equals("isContentAdmin")) {
